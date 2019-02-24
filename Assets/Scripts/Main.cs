8053c390b92559c41a22e1be42b7f7daf2a11239
zhenzhu143321 <hxcisunli@126.com>
2019-02-24 20:09:23 +0800

    添加一个脚本文件Main
    该脚本绑定在Main Camera上！

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Create a native array of a single float to store the result. This example waits for the job to complete for illustration purposes
+        NativeArray<float> result = new NativeArray<float>(1, Allocator.TempJob);
+
+        // Set up the job data
+        MyJob jobData = new MyJob();
+        jobData.a = 10;
+        jobData.b = 10;
+        jobData.result = result;
+
+        // Schedule the job
+        JobHandle handle = jobData.Schedule();
+
+        // Wait for the job to complete
+        handle.Complete();
+
+        // All copies of the NativeArray point to the same memory, you can access the result in "your" copy of the NativeArray
+        float aPlusB = result[0];
+
+        // Free the memory allocated by the result array
+        result.Dispose();
+        Debug.Log(aPlusB);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
