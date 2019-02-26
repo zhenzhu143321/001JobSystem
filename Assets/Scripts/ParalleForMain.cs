b29898dd51e8743ba6df4a7b4af1c87b238b4809
zhenzhu143321 <hxcisunli@126.com>
2019-02-26 13:18:34 +0800

    并行工作代码
    并行工作代码

---
+++ Assets/Scripts/ParalleForMain.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class ParalleForMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        NativeArray<float> a = new NativeArray<float>(2, Allocator.TempJob);
+
+        NativeArray<float> b = new NativeArray<float>(2, Allocator.TempJob);
+
+        NativeArray<float> result = new NativeArray<float>(2, Allocator.TempJob);
+
+        a[0] = 1.1f;
+        b[0] = 2.2f;
+        a[1] = 3.3f;
+        b[1] = 4.4f;
+
+        MyParallelJob jobData = new MyParallelJob();
+        jobData.a = a;
+        jobData.b = b;
+        jobData.result = result;
+
+        // Schedule the job with one Execute per index in the results array and only 1 item per processing batch
+        JobHandle handle = jobData.Schedule(result.Length, 1);
+
+        // Wait for the job to complete
+        handle.Complete();
+
+        // Free the memory allocated by the arrays
+        a.Dispose();
+        b.Dispose();
+        result.Dispose();
+    }
+
+}
---
+++ Assets/Scripts/ParallelForJobs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+// Job adding two floating point values together
+public struct MyParallelJob : IJobParallelFor
+{
+    [ReadOnly]
+    public NativeArray<float> a;
+    [ReadOnly]
+    public NativeArray<float> b;
+    public NativeArray<float> result;
+
+    public void Execute(int i)
+    {
+        result[i] = a[i] + b[i];
+    }
+}
